967a6faef2fe5c87502f03402298101aa6842950
TTXSTM <71548215+TTXSTM@users.noreply.github.com>
2021-03-20 23:38:26 +0300

    xxfd

--- Lesson01/HomeWork/Orange.java
+++
@@ -1,8 +0,0 @@
-package Lesson01.HomeWork;
-
-public class Orange extends Fruit {
-
-    public Orange() {
-        super(1.5f);
-    }
-}
--- Lesson01/HomeWork/WrongPositionException.java
+++
@@ -1,7 +0,0 @@
-package Lesson01.HomeWork;
-
-public class WrongPositionException extends Exception {
-    public WrongPositionException(String message) {
-        super(message);
-    }
-}
